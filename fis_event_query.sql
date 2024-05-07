412db508663216629742b0629268fc54db0ff354
Zach Cramer <121065404+za-cramer@users.noreply.github.com>
2024-05-07 08:58:54 -0600

    Update fis_event_query.sql
    added NEXT_ELIGIBLE_SABBATICAL. Logic stating that if TTT was hired, or accumulated service, of 6 years or greater they are eligible in current AY

--- fis_event_query.sql
+++ fis_event_query.sql
@@ -1,238 +1,88 @@
-/*
--- FIS SABBATICAL RECORDS --
-- Created by ZAC 04/2024
-- Purpose: Extracts first and most recent sabbatical events from FIS EVENT table
-- To Do:
-	[x] Caputure Event AY
+***************************************
+		SET TERM AND LIBRARY
+***************************************;
+%let year = 2023;
+%let first_year = %eval(&year. - 10);
+%let pending_events = %str('Leave Paid' 'Admin Appt' 'Leave Without Pay' 'Sabbatical');
+%let omitted_jobs = %str('1300','1448','1452','1453','1454','1455','1456','1601');
+%let ttt = %str('1100','1101','1102','1103', '1202', '1202', '1203');
+%let jobs = %str('1100','1101','1102','1103', '1202', '1202', '1203'); * Added Clinical (11/30/23);
+/* Admin Job Codes per Roster Definitions File Linked Above */
+%let admin = %str('2214','1446','2209','2208','2206','2210','2205','2207');
+/*  
+ 	1433 Faculty Director - Removed for Sabbatical Eligibility per Cholpon communication 5.6 to ZC and RS
+ 	1446 Director-Institute
+ 	2205 Chancellor
+ 	2206 Executive Vice Chancellor/VP
+ 	2207 Provost
+ 	2208 Executive Vice Chancellor
+ 	2209 Vice Chancellor
+ 	2210 Assoc Vice Chancellor
+ 	2214 Dean
 */
 
-WITH 
-	fis_extract AS
-		(SELECT 
-		  e.FIS_ID
-		, pers.EMPLID AS EID -- Include EID AS it will JOIN TO HCM DATA.
-		, e.EVENT_BEGIN_DATE
-		, e.EVENT_END_DATE
-		, e.POSITION_NUMBER
-		, e.DEPT_ID
-		, e.EVENT_JOBCLASS
-		, e.EVENT_DESC
-		FROM FACULTY.FIS_EVENT e
-		LEFT JOIN 
-			(SELECT DISTINCT
-				FIS_ID, EMPLID
-				FROM FACULTY.FIS_PERSON	-- EID comes from FIS_PERSON		
-			) pers
-			ON e.FIS_ID = pers.FIS_ID -- 1:1 MATCH 
-	ORDER BY  pers.EMPLID, e.EVENT_BEGIN_DATE	
-	)
-	,
-	sabbatical AS 
-		(SELECT 
-		  FIS_ID
-		, EID
-		, ROW_NUMBER() -- Identify ROW NUMBER FOR Individual ordered BY Event DATE 
-			OVER(PARTITION BY FIS_ID ORDER BY FIS_ID, EVENT_BEGIN_DATE) AS Record_Number
-		, EXTRACT(MONTH FROM EVENT_BEGIN_DATE) AS EFFECTIVE_MONTH
-		, EXTRACT(YEAR FROM EVENT_BEGIN_DATE)  AS EFFECTIVE_YEAR
-		, EXTRACT(MONTH FROM EVENT_END_DATE)  AS EXPIRE_MONTH
-		, EXTRACT(YEAR FROM EVENT_END_DATE)  AS EXPIRE_YEAR
-		, CASE 
-			WHEN EXTRACT(MONTH FROM EVENT_BEGIN_DATE)
-				BETWEEN 1 AND 7 
-				THEN 
-				/* Academic Year is prior calendar year if Spring Term */
-					CONCAT(CONCAT(CAST((EXTRACT(YEAR FROM EVENT_BEGIN_DATE) - 1) AS VARCHAR(10))
-					, ' - ')
-					, 	   CAST((EXTRACT(YEAR FROM EVENT_BEGIN_DATE)) AS VARCHAR(10)))
-			WHEN EXTRACT(MONTH FROM EVENT_BEGIN_DATE)
-				BETWEEN 8 AND 12 
-				THEN 
-					CONCAT(CONCAT(CAST((EXTRACT(YEAR FROM EVENT_BEGIN_DATE))AS VARCHAR(10)) 
-					, ' - ')
-					,CAST((EXTRACT(YEAR FROM EVENT_BEGIN_DATE) + 1)AS VARCHAR(10)) )
-			END AS AY
-		, CASE 
-			WHEN EXTRACT(MONTH FROM EVENT_BEGIN_DATE)
-				BETWEEN 1 AND 7 
-				THEN 
-				/* Academic Year Fall is prior calendar year if Spring Term */
-					(EXTRACT(YEAR FROM EVENT_BEGIN_DATE) - 1)
-				/* Academic Year Fall is current calendar year */
-			ELSE 	(EXTRACT(YEAR FROM EVENT_BEGIN_DATE))
-		  END AS AY_FALL
-		, EVENT_BEGIN_DATE
-		, EVENT_END_DATE
-		, POSITION_NUMBER
-		, DEPT_ID
-		, EVENT_JOBCLASS
-		, EVENT_DESC
-		FROM fis_extract 
-		WHERE EVENT_DESC = 'Sabbatical' -- 'Sabbatical' Events IN FIS_EVENT
-			AND EVENT_JOBCLASS IN ('1100', '1101', '1102', '1103') -- TTT JobCodes (Professors, Asst, & Assoc.)
-		ORDER BY  EID, EVENT_BEGIN_DATE)
-    ,
-	last_sabbatical AS 
-		(SELECT  FIS_ID
-				, EID
-				, Record_Number
-				, EFFECTIVE_MONTH
-				, EFFECTIVE_YEAR
-				, EXPIRE_MONTH
-				, EXPIRE_YEAR
-				, AY
-				, AY_FALL
-				, EVENT_BEGIN_DATE
-				, EVENT_END_DATE
-				, POSITION_NUMBER
-				, DEPT_ID
-				, EVENT_JOBCLASS
-				, EVENT_DESC
-			, 1 AS sabbatical
-			FROM sabbatical
-			WHERE (FIS_ID, Record_Number) IN 
-				(
-				SELECT FIS_ID, MAX(Record_Number)
-				FROM sabbatical
-				GROUP BY FIS_ID
-				)
-		)
-,	
-	next_eligible AS
-	(SELECT FIS_ID
-	   , EID
-	   , Record_Number
-	   , EFFECTIVE_MONTH
-	   , EFFECTIVE_YEAR
-	   , EXPIRE_MONTH
-	   , EXPIRE_YEAR
-	   , 
-		CASE 
-			WHEN EXTRACT(MONTH FROM EVENT_BEGIN_DATE)
-				BETWEEN 1 AND 7 
-				THEN 
-				/* Academic Year is prior calendar year if Spring Term */
-					CONCAT(CONCAT(CAST((EXTRACT(YEAR FROM EVENT_BEGIN_DATE) - 1) +6 AS VARCHAR(10))
-					, ' - ')
-					, 	   CAST((EXTRACT(YEAR FROM EVENT_BEGIN_DATE)) +6 AS VARCHAR(10)))
-			WHEN EXTRACT(MONTH FROM EVENT_BEGIN_DATE)
-				BETWEEN 8 AND 12 
-				THEN 
-					CONCAT(CONCAT(CAST((EXTRACT(YEAR FROM EVENT_BEGIN_DATE))+6 AS VARCHAR(10)) 
-					, ' - ')
-					,CAST((EXTRACT(YEAR FROM EVENT_BEGIN_DATE) + 1)+6 AS VARCHAR(10)) )
-		END AS AY
-	, NULL AS AY_FALL
-	, EVENT_BEGIN_DATE
-	, EVENT_END_DATE
-	, POSITION_NUMBER
-	, DEPT_ID
-	, EVENT_JOBCLASS
-	, EVENT_DESC
-	, sabbatical
-FROM last_sabbatical	
-	)
-,
-	no_sabbatical_all AS 
-		(SELECT 
-		  FIS_ID
-		, EID
-		, ROW_NUMBER()
-			OVER(PARTITION BY FIS_ID ORDER BY FIS_ID, EVENT_BEGIN_DATE) AS Record_Number
-		, EXTRACT(MONTH FROM EVENT_BEGIN_DATE) AS EFFECTIVE_MONTH
-		, EXTRACT(YEAR FROM EVENT_BEGIN_DATE)  AS EFFECTIVE_YEAR
-		, EXTRACT(MONTH FROM EVENT_END_DATE)  AS EXPIRE_MONTH
-		, EXTRACT(YEAR FROM EVENT_END_DATE)  AS EXPIRE_YEAR
-		, CASE 
-			WHEN EXTRACT(MONTH FROM EVENT_BEGIN_DATE)
-				BETWEEN 1 AND 7 
-				THEN 
-				/* Academic Year is prior calendar year if Spring Term */
-					CONCAT(CONCAT(CAST((EXTRACT(YEAR FROM EVENT_BEGIN_DATE) - 1) AS VARCHAR(10))
-					, ' - ')
-					, 	   CAST((EXTRACT(YEAR FROM EVENT_BEGIN_DATE)) AS VARCHAR(10)))
-			WHEN EXTRACT(MONTH FROM EVENT_BEGIN_DATE)
-				BETWEEN 8 AND 12 
-				THEN 
-					CONCAT(CONCAT(CAST((EXTRACT(YEAR FROM EVENT_BEGIN_DATE))AS VARCHAR(10)) 
-					, ' - ')
-					,CAST((EXTRACT(YEAR FROM EVENT_BEGIN_DATE) + 1)AS VARCHAR(10)) )
-			END AS AY
-		, CASE 
-			WHEN EXTRACT(MONTH FROM EVENT_BEGIN_DATE)
-				BETWEEN 1 AND 7 
-				THEN 
-				/* Academic Year Fall is prior calendar year if Spring Term */
-					(EXTRACT(YEAR FROM EVENT_BEGIN_DATE) - 1)
-				/* Academic Year Fall is current calendar year */
-			ELSE 	(EXTRACT(YEAR FROM EVENT_BEGIN_DATE))
-		  END AS AY_FALL
-		, CASE 
-			WHEN EXTRACT(MONTH FROM EVENT_BEGIN_DATE)
-				BETWEEN 1 AND 7 
-				THEN 
-				/* Academic Year is prior calendar year if Spring Term */
-					CONCAT(CONCAT(CAST((EXTRACT(YEAR FROM EVENT_BEGIN_DATE) - 1) +6 AS VARCHAR(10))
-					, ' - ')
-					, 	   CAST((EXTRACT(YEAR FROM EVENT_BEGIN_DATE)) +6 AS VARCHAR(10)))
-			WHEN EXTRACT(MONTH FROM EVENT_BEGIN_DATE)
-				BETWEEN 8 AND 12 
-				THEN 
-					CONCAT(CONCAT(CAST((EXTRACT(YEAR FROM EVENT_BEGIN_DATE))+6 AS VARCHAR(10)) 
-					, ' - ')
-					,CAST((EXTRACT(YEAR FROM EVENT_BEGIN_DATE) + 1)+6 AS VARCHAR(10)) )
-		END AS NEXT_ELIGIBLE_SABBATICAL
-		, EVENT_BEGIN_DATE
-		, NULL AS EVENT_END_DATE
-		, POSITION_NUMBER
-		, DEPT_ID
-		, EVENT_JOBCLASS
-		, EVENT_DESC
-		, 0 AS sabbatical
-		FROM fis_extract 
-		WHERE FIS_ID NOT IN 
-			(
-			SELECT DISTINCT FIS_ID FROM fis_extract WHERE EVENT_DESC = 'Sabbatical'
-			)
-		AND 
-			EVENT_DESC = 'Appointment'
-		AND 
-			EVENT_JOBCLASS IN ('1100', '1101', '1102', '1103')
-		ORDER BY  EID, EVENT_BEGIN_DATE)
-, 	no_sabbatical AS
-	(
-		SELECT * FROM no_sabbatical_all
-		WHERE Record_Number = 1
-		)	
-,  sabbatical_final AS 
-	(SELECT 
-	  LAST.FIS_ID
-	, LAST.EID
-	, LAST.RECORD_NUMBER
-	, LAST.EFFECTIVE_MONTH
-	, LAST.EFFECTIVE_YEAR
-	, LAST.EXPIRE_MONTH
-	, LAST.EXPIRE_YEAR
-	, LAST.AY
-	, LAST.AY_FALL
-	, NEXT.AY AS NEXT_ELIGIBLE_SABBATICAL
-	, LAST.EVENT_BEGIN_DATE
-	, LAST.EVENT_END_DATE
-	, LAST.POSITION_NUMBER
-	, LAST.DEPT_ID
-	, LAST.EVENT_JOBCLASS
-	, LAST.EVENT_DESC
-	, LAST.SABBATICAL 
-FROM last_sabbatical LAST
-	LEFT JOIN next_eligible NEXT
-	ON LAST.fis_id = NEXT.FIS_ID
-ORDER BY LAST.EID
-	)
-SELECT *
-FROM sabbatical_final	
-UNION
-SELECT 	*
-FROM no_sabbatical
-;
+%put <<<< snapyear = &year. || first_year = &first_year.;
 
-		
+libname lib 'L:\IR\facstaff\OFA\Sabbatical Report';
+
+%include 'L:\IR\facstaff\OFA\Retention Dashboard\bi-annual_appts.sas'; 
+
+/* Import FIS Sabbatical Events query found L:\IR\facstaff\OFA\Sabbatical Report\sabbatical_eligibility.sql */
+proc import datafile="L:\IR\facstaff\OFA\Sabbatical Report\fis_sabbatical.csv"
+     out=fis_sabbatical
+     dbms=csv
+     replace;
+     getnames=yes;
+run;
+proc contents data = fis_sabbatical; run;
+
+/* Import CIW Leave Events query found L:\IR\facstaff\OFA\Sabbatical Report\ciw_leave.sql */
+proc import datafile="L:\IR\facstaff\OFA\Sabbatical Report\ciw_leave.csv"
+     out=ciw_leave
+     dbms=csv
+     replace;
+     getnames=yes;
+run;
+proc contents data = ciw_leave; run;
+
+************* BEGIN ANALYSIS *********************;
+* Employed Faculty 
+* Remove Retirees, retain active TTT;
+%ciwdb;
+proc sql;
+	create table active_sabbatical as
+	select sab.EID,
+		   hr.EMPLOYEE_NAME,
+		   pers.admin as Current_Admin,
+		   sab.SABBATICAL as Prior_Sabbatical_Flag,
+		   sab.EFFECTIVE_MONTH,
+		   sab.EFFECTIVE_YEAR,
+		   sab.EXPIRE_MONTH,
+		   sab.EXPIRE_YEAR,
+		   sab.AY,
+		   sab.AY_FALL,
+		   sab.NEXT_ELIGIBLE_SABBATICAL as NEXT_ELIGIBLE_SABBATICAL_OLD,
+		   case
+		   	when (&year. - sab.AY_FALL)  >= 6  /* Logic says if they were hired, or accumulated service, of 6 years or greater they are eligible */
+				 then catx(' - ', put(&year., 4.), put(&year. + 1, 4.)) 
+				 else sab.NEXT_ELIGIBLE_SABBATICAL
+			end as NEXT_ELIGIBLE_SABBATICAL 
+	from fis_sabbatical sab /* Imported Sabbatical Roster */
+		left join ciwdb.HRMS_PERSONAL_TBL hr /* Join HR Name */
+			on sab.EID = hr.EMPLOYEE_ID
+		left join
+			(select distinct 
+					EID, 
+					1 as admin /* 'Admin' flag based on Cholpon's list */
+				from edb.appts&year.
+				where jobcode in (&admin.)) pers
+			on sab.EID = pers.EID
+	where sab.EID in
+		(select 
+			distinct EID 
+			from edb.appts&year.
+		 where JobCode not like '16%'
+			and Big3)
+	order by EFFECTIVE_YEAR;
+quit;
+			
